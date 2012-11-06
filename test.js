5ce2b08c0dc9ae499a46c3e55d3af70ec8a15811
mduca <michael@mandu.ca>
2012-11-05 22:58:59 -0600

    testing jQuery loading

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ alert("From test.js");
 if (typeof jQuery == 'undefined') {
   var jQ = document.createElement('script');
   jQ.type = 'text/javascript';
-  jQ.onload=getDependencies;
   jQ.src = 'http://code.jquery.com/jquery-1.7.2.min.js';
   document.body.appendChild(jQ);
 }
