871452192ef258d570cfc03e788e8a692697dfb6
Michael <michael@mandu.ca>
2012-11-06 01:36:00 -0600

    update injecter again

--- injectDepend.js
+++ injectDepend.js
@@ -3,6 +3,9 @@ script.src = "http://code.jquery.com/jquery-1.7.2.min.js";
 script.type = "text/javascript";
 script.onload = function() {
   jQuery.noConflict();
+  var barHtml = "<div id='hermes' class='navbar' style='width: 340px;'><div class='navbar-inner'><div class='container' style='width: auto;'><a class='brand' href='#'>Hermes</a><div class='btn-group' data-toggle='buttons-radio'><button id='btnStart' class='btn btn-success'>Start</button><button id='btnStop' class='btn btn-danger'>Stop</button></div><ul class='nav pull-right' role='navigatoin'><li class='dropdown'><a id='settings' href='#' role='button' class='dropdown-toggle' data-toggle='dropdown'>Settings<b class='caret'></b></a><ul class='dropdown-menu'><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 1</a></li><li class='divider'></li><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 2</a></li></ul></li></ul></div></div></div>";
+
+   jQuery("body").append(barHtml);
 }
 document.getElementsByTagName('head')[0].appendChild(script);
 
@@ -22,17 +25,8 @@ script.onload = function() {
 }
 document.getElementsByTagName('head')[0].appendChild(script);
 
-var script = document.createElement('script');
-script.src = "http://raw.github.com/mduca/bookmarklets/master/hermes.prototype.js";
-script.type = "text/javascript";
-script.onload = function() {
-  jQuery.noConflict();
-}
-document.getElementsByTagName('head')[0].appendChild(script);
-
 
 // CSS
-
 var css = document.createElement('link');
 css.href = "http://twitter.github.com/bootstrap/assets/css/bootstrap.css";
 css.rel = "stylesheet";
