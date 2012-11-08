a2617ff198fde9f6774cdfd20c1fed69b12e9fcc
mduca <michael@mandu.ca>
2012-11-07 22:54:16 -0600

    if this works I'm happy

--- injectDepend.js
+++ injectDepend.js
@@ -35,7 +35,8 @@ script.onload = function() {
   var barHtml = "<div id='hermes' class='navbar' style='width: 340px;'><div class='navbar-inner'><div class='container' style='width: auto;'><a class='brand' href='#'>Hermes</a><div class='btn-group' data-toggle='buttons-radio'><button id='btnStart' class='btn btn-success'>Start</button><button id='btnStop' class='btn btn-danger'>Stop</button></div><ul class='nav pull-right' role='navigatoin'><li class='dropdown'><a id='settings' href='#' role='button' class='dropdown-toggle' data-toggle='dropdown'>Settings<b class='caret'></b></a><ul class='dropdown-menu'><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 1</a></li><li class='divider'></li><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 2</a></li></ul></li></ul></div></div></div>";
 
   jQuery("body").append(barHtml);
-  jQuery("#hermes").draggable();
-}
+  }
 
 document.getElementsByTagName('head')[0].appendChild(script);
+jQuery("#hermes").draggable();
+
