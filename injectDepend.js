5da60400ec28c69842d63916113c07ad2389eac8
mduca <michael@mandu.ca>
2012-11-07 20:19:56 -0600

    broke code into bite size functions, yummy. BURP!

--- injectDepend.js
+++ injectDepend.js
@@ -1,9 +1,23 @@
 
-function fileType(url) {
-  return url.split('.').pop();
+var resources = [ "http://twitter.github.com/bootstrap/assets/css/bootstrap.css",
+                  "http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css",
+                  "http://twitter.github.com/bootstrap/assets/js/bootstrap.js",
+                  "http://code.jquery.com/ui/1.9.1/jquery-ui.min.js",
+                  ]
+
+function fileType(file) {
+  return file.split('.').pop();
 }
 
-function addCss(url) {
+function jsOrCss(file) {
+  fileType(file) === 'js' ? createScriptTag(file) : createLinkTag(file);
+}
+
+function addToHead(addMe) {
+  document.getElementsByTagName('head')[0].appendChild(addMe);
+}
+
+function createLinkTag(url) {
   var css = document.createElement('link');
   css.href = url;
   css.rel = "stylesheet";
@@ -11,8 +25,8 @@ function addCss(url) {
   return css;
 }
 
-function addJs(url, load) {
-  var js = document.createElement('script');
+function createScriptTag(url, load) {
+  var script = document.createElement('script');
   script.src = url;
   script.type = "text/javascript";
   if (load) {
@@ -20,45 +34,16 @@ function addJs(url, load) {
   }
 }
 
-// CSS
-var css = document.createElement('link');
-css.href = "http://twitter.github.com/bootstrap/assets/css/bootstrap.css";
-css.rel = "stylesheet";
-css.type = "text/css";
-document.getElementsByTagName('head')[0].appendChild(css);
-
-var css = document.createElement('link');
-css.href = "http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css";
-css.rel = "stylesheet";
-css.type = "text/css";
-
-//JavaScript
-document.getElementsByTagName('head')[0].appendChild(css);var script = document.createElement('script');
-script.src = "http://code.jquery.com/jquery-1.7.2.min.js";
-script.type = "text/javascript";
-script.onload = function() {
+var jq = createScriptTage("http://code.jquery.com/jquery-1.7.2.min.js", function() {
   jQuery.noConflict();
   var barHtml = "<div id='hermes' class='navbar' style='width: 340px;'><div class='navbar-inner'><div class='container' style='width: auto;'><a class='brand' href='#'>Hermes</a><div class='btn-group' data-toggle='buttons-radio'><button id='btnStart' class='btn btn-success'>Start</button><button id='btnStop' class='btn btn-danger'>Stop</button></div><ul class='nav pull-right' role='navigatoin'><li class='dropdown'><a id='settings' href='#' role='button' class='dropdown-toggle' data-toggle='dropdown'>Settings<b class='caret'></b></a><ul class='dropdown-menu'><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 1</a></li><li class='divider'></li><li><a href='#' rel='popover' placement='right' data-content='This is the settings popover dialog'>Option 2</a></li></ul></li></ul></div></div></div>";
 
    jQuery("body").append(barHtml);
    jQuery("#hermes").draggable();
+  })
+
+addToHead(jq);
+// Main Loop
+for (i = 0; i < resources.length; i++) {
+  addToHead( jsOrCss( resources[i] ) );
 }
-document.getElementsByTagName('head')[0].appendChild(script);
-
-var script = document.createElement('script');
-script.src = "http://code.jquery.com/ui/1.9.1/jquery-ui.min.js";
-script.type = "text/javascript";
-script.onload = function() {
-  jQuery.noConflict();
-}
-document.getElementsByTagName('head')[0].appendChild(script);
-
-var script = document.createElement('script');
-script.src = "http://twitter.github.com/bootstrap/assets/js/bootstrap.js";
-script.type = "text/javascript";
-script.onload = function() {
-  jQuery.noConflict();
-}
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
