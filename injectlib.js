1f889ca48975635d453ddaf76fd2123865d63d31
mduca <michael@mandu.ca>
2012-11-19 13:31:15 -0600

    reorganized project

--- injectlib.js
+++ injectlib.js
@@ -1,19 +1,18 @@
-alert("From for.js");
 var script = document.createElement('script');
 script.src = "http://code.jquery.com/jquery-1.7.2.min.js";
 script.type = "text/javascript";
 script.onload = function() {
   jQuery.noConflict();
   
-  var resources = [ "http://twitter.github.com/bootstrap/assets/css/bootstrap.css",
-                  "http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css",
-                  "http://twitter.github.com/bootstrap/assets/js/bootstrap.js",
-                  "http://code.jquery.com/ui/1.9.1/jquery-ui.min.js",
-                  ];
+  var resources = [ 
+    "http://twitter.github.com/bootstrap/assets/css/bootstrap.css"
+  , "http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css"
+  , "http://twitter.github.com/bootstrap/assets/js/bootstrap.js"
+  , "http://code.jquery.com/ui/1.9.1/jquery-ui.min.js"
+  ];
 
-  resources.forEach(function(blah) {
-    alert(blah);
-    document.head.appendChild(jsOrCss(blah));
+  resources.forEach(function(resource) {
+    document.head.appendChild(jsOrCss(resource));
   });
 
   function jsOrCss(file) {
@@ -38,6 +37,8 @@ script.onload = function() {
     script.type = "text/javascript";
     return script;
   }
-}
+};
 
 document.head.appendChild(script);
+
+
